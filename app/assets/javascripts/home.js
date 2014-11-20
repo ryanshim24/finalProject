3e03c7850e610169bcf44aba786200c00460659e
Ryan Shim <ryanshim92@gmail.com>
2014-11-19 19:12:48 -0800

    Got it going on

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+  $('#directions-panel').hide();
+  $('.secondary').hide();
+  
+
   // MY Forget Password Modal
   $(".reset-link").on("click", function() {
     $('#logModal').modal('hide');
@@ -8,6 +12,22 @@ $(document).ready(function(){
   //Autofocus for the Modals
   $('.modal').on('shown.bs.modal', function() {
     $(this).find('[autofocus]').focus();
+  });
+
+  
+
+
+  $(".travelSearch").on('submit', function(e){
+    e.preventDefault();
+    displayMap();
+    calcRoute();
+    $('#directions-panel').show();
+    $('.secondary').show();
+    $("body").animate({scrollTop: $("#map-canvas").offset().top }, 2000);
+    $('#origin').val("");
+    $('#destination').val("");
+    $('#dateStart').val("");
+    $('#dateEnd').val("");
   });
 
 
@@ -40,59 +60,54 @@ $(document).ready(function(){
       }
     });
 
-    // $(".travelSearch").on('submit', function(e){
-    //   e.preventDefault();
-    //   var origin = $('#origin').val();
-    //   var budget = $('#budget').data('slider').getValue();
-    //   var startDate = $('#dateStart').val();
-    //   var endDate = $('#dateEnd').val();
-
-    //   console.log(origin);
-    //   console.log(budget);
-    //   console.log(startDate);
-    //   console.log(endDate);
-    // });
 
 
-// QbXNQXcrtZdN5ktL.
+// this is MAP 
+
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+
+    function displayMap() {
+      document.getElementById('map-canvas').style.display="block";
+      initialize();
+    }
+
+    function initialize() {
+      directionsDisplay = new google.maps.DirectionsRenderer();
+      var mapOptions = {
+        zoom: 7,
+        center: new google.maps.LatLng(41.850033, -87.6500523)
+      };
+      var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+      directionsDisplay.setMap(map);
+      directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
+  // var control = document.getElementById('control');
+  // control.style.display = 'block';
+  // map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+    } 
+
+    function calcRoute() {
+      var start = document.getElementById('origin').value;
+      var end = document.getElementById('destination').value;
+      var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING
+      };
+      directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        }
+     });
+    }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
 
-
-
-    // function getDirections(origin,end){
-    //   // return $.getJSON("https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+end+"&key=AIzaSyB4YVLXv__XgNZ6HhzTsaUvaJQObIsMteE");
-    //   $.ajax({
-    //     type: 'GET',
-    //     url: "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+end+"&key=AIzaSyB4YVLXv__XgNZ6HhzTsaUvaJQObIsMteE",
-    //     dataType: 'jsonp',
-    //     success: function(result) {
-    //       console.log("results returned!");
-    //       dis = result;
-    //       result.routes.forEach(function(data){
-    //         $('.travelData').append(data.legs.duration.text);
-    //       });
-    //     }
-    //   });
-    // }
-
-    // $(".travelSearch").on('submit', function(e){
-    //   e.preventDefault();
-    //   var origin = $('#origin').val();
-    //   var end = $('#end').val(); 
-    //   getDirections(origin, end);
-
-    //   // $.when(getDirections(origin,end)).done(function(result){
-    //   //   console.log(result);
-    //   //   result.routes.forEach(function(data){
-    //   //     $('.travelData').append(data.legs.duration.text);
-    //   //   });
-    //   // });
-    // });
-
-
-// AIzaSyB4YVLXv__XgNZ6HhzTsaUvaJQObIsMteE
 
 
 });//DOCUMENT CLOSED
